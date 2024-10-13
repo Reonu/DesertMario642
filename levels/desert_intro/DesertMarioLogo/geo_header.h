e5323271a1b3bbef8d75eb5e3c9918584f615326
Reonu <danileon95@gmail.com>
2024-10-13 15:05:44 +0100

    Smaller logo

--- levels/desert_intro/DesertMarioLogo/geo_header.h
+++ levels/desert_intro/DesertMarioLogo/geo_header.h
@@ -1,6 +1,7 @@
+extern const GeoLayout DesertMarioLogo_geo[];
 extern u8 DesertMarioLogo_DesertMarioLogoIngame_rgba16[];
 extern Vtx DesertMarioLogo_DesertMarioLogo_mesh_layer_5_vtx_cull[8];
-extern Vtx DesertMarioLogo_DesertMarioLogo_mesh_layer_5_vtx_0[120];
+extern Vtx DesertMarioLogo_DesertMarioLogo_mesh_layer_5_vtx_0[140];
 extern Gfx DesertMarioLogo_DesertMarioLogo_mesh_layer_5_tri_0[];
 extern Gfx mat_DesertMarioLogo_logo[];
 extern Gfx mat_revert_DesertMarioLogo_logo[];
