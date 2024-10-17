bd4fb6d1bcbe076f5bc8a1e4a7688633d3e8d1c8
Reonu <danileon95@gmail.com>
2024-10-17 01:23:59 +0100

    fix pokey coins

--- src/game/behaviors/pokey.inc.c
+++ src/game/behaviors/pokey.inc.c
@@ -45,6 +45,7 @@ void bhv_pokey_body_part_update(void) {
     // PARTIAL_UPDATE
 
     if (o->parentObj == NULL || o->parentObj->activeFlags == ACTIVE_FLAG_DEACTIVATED || !obj_has_behavior(o->parentObj, bhvPokey)) {
+        obj_die_if_health_non_positive();
         obj_mark_for_deletion(o);
         return;
     }
